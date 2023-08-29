2708ca356b4e9f5b7f5880c93d3a58e8520a7af0
thaaisgonsales <thaaisgonsales@gmail.com>
2023-08-28 22:17:02 -0400

    desafio java basico

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,25 @@
-public class ContaTerminal{
+import java.util.Scanner;
+
+public class ContaTerminal {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor digite o numero da conta!");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agencia!");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor digite o seu nome!");
+        String titularNome = scanner.next();
+
+        System.out.println("Por favor digite o valor a depositar!");
+        double valorSaldo = scanner.nextDouble();
+
+        scanner.close();
+
+        System.out.println("Olá " + titularNome + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia +
+                ", conta " + numeroConta + ", e o seu saldo é " + valorSaldo + " já está disponível para saque.");
     }
 }
